4c25c2ea5898eb2f061251b895b29a3989ebe95c
Jerrod C Lepper <lepperj@oregonstate.edu>
2021-11-03 22:48:09 -0400

    init commit

---
+++ SearchNLearn/LocalAnimalTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LocalAnimalTableViewCell.swift
+//  SearchNLearn
+//
+//  Created by Jerrod on 11/3/21.
+//
+
+import UIKit
+
+class LocalAnimalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SearchNLearn/MapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapViewController.swift
+//  SearchNLearn
+//
+//  Created by Jerrod on 11/3/21.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func Logout(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
