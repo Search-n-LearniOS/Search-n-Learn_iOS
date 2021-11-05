09c68f11ae6c3907370890df8e14432a1a10cc4b
jahenner <jahenner@gmail.com>
2021-11-05 12:13:43 -0400

    Added Parse Config in appDelegate, and updated password field to be secure plus one autoconfig bug fixed

--- SearchNLearn/AppDelegate.swift
+++ SearchNLearn/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "AUia27MQC5SMhlOdBqXytzTzFyHJ1TJLJX0Yo3rp"
+            $0.clientKey = "DwBYorPIV1YOP4bI1vTxY9IEKRxCe9zWkiu5owJH"
+            $0.server = "https://parseapi.back4app.com"
+        }
         return true
     }
 
