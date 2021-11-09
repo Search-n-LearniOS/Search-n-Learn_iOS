56bae2308821a12eab50ac903c282e457669f00c
tom <thomas.mcnutt.jr@gmail.com>
2021-11-08 21:01:00 -0500

    Changes by McNutt

---
+++ SearchNLearn/MyAnimalCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyAnimalCollectionViewCell.swift
+//  SearchNLearn
+//
+//  Created by thomas mcnutt on 11/7/21.
+//
+
+import UIKit
+
+class MyAnimalCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var MyAnimalImageView: UIImageView!
+}
